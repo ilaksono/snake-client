b871c1a229e74c6a671662e9e3ae0e929508284a
ilaksono <ian.laksono@gmail.com>
2020-09-22 23:35:02 +0000

    added name sending

--- client.js
+++ client.js
@@ -12,7 +12,7 @@ const connect = function() {
   })
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    conn.write(('Name: IAN'));
+    conn.write('Name: IAN');
   })
   return conn;
 }
