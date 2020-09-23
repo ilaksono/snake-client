48a7fcafad1272bb919418f7fc2b24e55bcf967e
ilaksono <ian.laksono@gmail.com>
2020-09-23 01:37:02 +0000

    added readline event listener for SIGINT

--- play.js
+++ play.js
@@ -1,4 +1,31 @@
+const { SIGINT } = require('constants');
+const readline = require('readline');
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  rl.on('data', (data) => {
+    handleUserInput(data);
+  })
+  return stdin;
+};
+
+const handleUserInput = (data) => {
+  if (data === 'SIGINT') {
+    console.log('Exiting client');
+    process.exit();
+  }
+  
+};
 
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+setupInput();
+
